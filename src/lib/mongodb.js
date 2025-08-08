8a3d5fe39cf65c3c535a2cc9a881435e1d2c575a
Piotr Nagórny <pinagotr@gmail.com>
2025-08-08 22:35:19 +0200

    fix related to database and added helping .env.local file

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -12,16 +12,3 @@ export const client = new MongoClient(uri,  {
         }
     }
 );
-
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
-  }
-}
-
-
-run().catch(console.dir);
