ec772632229766db42ae47356d7554ba7a62820d
pikoseq9 <wekpat@wp.pl>
2025-08-10 10:53:38 +0200

    add apiform page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import styles from "./page.module.css";
 import Chat from "../components/Chat.js";
 import Card from "../components/Card.js";
-import ApiKeyForm from "@/components/ApiKeyForm";
 
 export default function Home() {
   return (
--- src/components/ApiKeyForm.js
+++ src/components/ApiKeyForm.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import styles from "./ApiKeyForm.module.css";
 
 export default function ApiKeyForm({ onSubmitApiKey }) {
   const [newApiKey, setNewApiKey] = useState("");
@@ -12,14 +13,17 @@ export default function ApiKeyForm({ onSubmitApiKey }) {
   };
 
   return (
-    <form className="" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Type in your API key"
-        value={newApiKey}
-        onChange={(e) => setNewApiKey(e.target.value)}
-      />
-      <button type="submit">Użyj klucza</button>
-    </form>
+    <div className={styles["api-key-form-box"]}>
+      <h1>Give your API Key</h1>
+      <form className={styles["api-key-form"]} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Type in your API key"
+          value={newApiKey}
+          onChange={(e) => setNewApiKey(e.target.value)}
+        />
+        <button type="submit">Użyj klucza</button>
+      </form>
+    </div>
   );
 }
